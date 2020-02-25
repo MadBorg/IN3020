715d920a88e58a72e525b8d5baccc13d1f158f31
madborg <rednas.1604@gmail.com>
2020-02-25 15:04:55 +0100

    added insert, query, insert to dbm, with example in oppg1

--- oblig1/src/dbm.py
+++ oblig1/src/dbm.py
@@ -30,11 +30,12 @@ class DBM():
         # attributes
         query += "\n"
         for attr in attributes[:-1]:
-            query += _attr(attr, last=False)
-        query += _attr(attributes[-1], last=True)       
+            query += self._attr(attr, last=False)
+        query += self._attr(attributes[-1], last=True)       
         
         # Primary key
         if primary_key:
+            self._primary_key = primary_key
             query += ",\n"
             query += " PRIMARY KEY"
             if type(primary_key) is tuple or type(primary_key) is list:
@@ -72,42 +73,108 @@ class DBM():
         with self.conn:  # With a context manager we don't need a commit
             self.cur.execute(query)
 
+    def insert(self, table, data):
+        """
+        Insert to a existing table
+        """
 
+        # Init
+        attributes = list(data[0]) # listing all attributes from data
+        # Asumtions
+        assert table in self.tables, "Table is not regisered."
+
+        # data
+        insert_header = f"INSERT INTO {table}\n"
+        for dp in data:
+            q = insert_header
+            q += "VALUES ("
+            for attr in attributes[:-1]:
+                q += f":{attr}, "
+            q += f":{attributes[-1]})"
+            with self.conn:
+                self.cur.execute(q, dp)        
+
+    def query(self, q, data = None):
+        with self.conn:
+            return self.cur.execute(q, data)
+        # if data:
+        #     with self.conn:
+        #         return self.cur.execute(q, data)
+        # with self.conn:
+        #     self.cur.execute(q)
     def close(self):
         self.conn.close()
 
+    def check_specific_insert(self, table, data):
+        """
+        Testing if insert with equivalent input worked, Just with print so far
 
-def _attr(attr, last = False):
-    q = "" # query
-    attr_name, attr_datatype = attr["name"], attr["datatype"]
-    q += f"{attr_name} {attr_datatype}"
+        """
+        # Asserts
 
-    if "primary_key" in attr:
-        attr_primary_key = attr["primary_key"]
-        assert type(attr_primary_key) is bool, "primary_key is not boolean"
-        if attr_primary_key:
-            q += " PRIMARY KEY"
-    else:
-        if "null" in attr:
-            null = attr["null"]
-            assert type(null) is bool, "null is not bool"
-            if not null:
-                q += " NOT NULL"
-        if "default" in attr:
-            default = attr["default"]
-            assert type(default) is str, "default is not str"
-            q += f" DEFAULT {default} "
-        if "unique" in attr:
-            unique = attr[unique]
-            assert type(unique) is bool
-            if unique:
-                q += " UNIQUE"
-    # Ending attr
-    if not last:
-        q += ", \n"
-    elif last:
-        q += ""
-    return q
+        # Init
+        attributes = list(data[0])
+        
+        # prossesing
+        # Select
+        attr_str = ""
+        for attr in attributes[:-1]:
+            attr_str += f"{attr}, "
+        attr_str += f"{attributes[-1]}"
+        select_str = "SELECT " + attr_str
+        # From
+        from_str = f"FROM {table}"
+        # Building the query
+        query = select_str + "\n" + from_str
+
+        # Query
+        with self.conn:
+            res = self.cur.execute(query)
+        res_data = res.fetchall()
+        print(f"check_specific_insert: \n{res_data}")
+
+
+
+        
+    # Properties
+    @property
+    def primary_key(self):
+        try:
+            return self._primary_key
+        except NameError:
+            raise NameError("Primary key not defined")
+
+    def _attr(self, attr, last = False):
+        q = "" # query
+        attr_name, attr_datatype = attr["name"], attr["datatype"]
+        q += f"{attr_name} {attr_datatype}"
+
+        if "primary_key" in attr:
+            attr_primary_key = attr["primary_key"]
+            assert type(attr_primary_key) is bool, "primary_key is not boolean"
+            if attr_primary_key:
+                q += " PRIMARY KEY"
+        else:
+            if "null" in attr:
+                null = attr["null"]
+                assert type(null) is bool, "null is not bool"
+                if not null:
+                    q += " NOT NULL"
+            if "default" in attr:
+                default = attr["default"]
+                assert type(default) is str, "default is not str"
+                q += f" DEFAULT {default} "
+            if "unique" in attr:
+                unique = attr[unique]
+                assert type(unique) is bool
+                if unique:
+                    q += " UNIQUE"
+        # Ending attr
+        if not last:
+            q += ", \n"
+        elif last:
+            q += ""
+        return q
 
 if __name__ == "__main__":
     pass
--- oblig1/src/oppg1.py
+++ oblig1/src/oppg1.py
@@ -1,6 +1,9 @@
-
-# 1.1 Self join
 import dbm as db
+import pandas as pd
+import numpy as np
+
+import IPython as ip
+# 1.1 Self join
 
 # Setup for On Stage Personell
 osp_name = "OnStagePersonnel"
@@ -16,7 +19,7 @@ sm_name = "StageManager"
 sm_attributes = (
      # name, datatype, null, primary key, default
      {"name": "ospID", "datatype": "INTEGER"},
-     {"name": "smID", "datatype": "INTEGER", "primary_key": True},
+     {"name": "smID", "datatype": "INTEGER"},
      {"name": "name", "datatype": "TEXT"},
      {"name": "lastname", "datatype": "TEXT"},
      {"name": "function", "datatype": "TEXT"} 
@@ -26,8 +29,55 @@ sm_references = (
      {"table": "OnStagePersonnel", "key": "ospID","forign_key": "ospID"},
 )
 
+sm_primary_key = ( # TODO : test
+     "ospID", "smID"
+)
 
+path_to_names_csv = "https://raw.githubusercontent.com/hadley/data-baby-names/master/baby-names.csv"
+names_df = pd.read_csv(path_to_names_csv, sep=',')
+
+
+name_counter = 0
 # Data for osp
+osp_N = 100
+osp_data = [
+     # {ospID: <int>, name: <str>, lastname: <str>}
+]
+
+firsname_data_osp = names_df["name"][name_counter:name_counter + osp_N].to_list()æ; name_counter += osp_N
+lastname_data_osp = names_df["name"][name_counter:name_counter + osp_N].to_list()æ; name_counter += osp_N
+start_id = 1000
+
+ospID = start_id
+for firstname, lastname in zip(firsname_data_osp, lastname_data_osp):
+     osp_data.append(
+          {"ospID": ospID, "name": firstname, "lastname": lastname}
+     )
+     ospID += 1
+
+# Data for sm
+sm_N = 20
+sm_data = [
+     # {ospID: <int>, smID: <int>, name: <str>, lastname: <str>, function: <str>}
+]
+
+# ip.embed()
+firsname_data_sm = names_df["name"][name_counter:name_counter + osp_N].to_list()æ; name_counter += sm_N
+lastname_data_sm = names_df["name"][name_counter:name_counter + osp_N].to_list()æ; name_counter += sm_N
+functions = ("rehearsals", "coordinating", "communicating", "overseeing", "calling cues")
+smIDs = np.random.randint(100, 1000, size=osp_N)
+function_indices = np.random.randint(0, len(functions), size=osp_N)
+
+# Assigning sm to all osp
+for i in range(osp_N):
+     sm_data.append({
+          "ospID": osp_data[i]["ospID"],
+          "smID": smIDs[i],
+          "name": firsname_data_sm[i],
+          "lastname": lastname_data_sm[i],
+          "function": functions[ function_indices[i] ]
+          })
+
 
 
 if __name__ == "__main__":
@@ -39,5 +89,21 @@ if __name__ == "__main__":
      db_obj.create_table(
           name=sm_name, attributes=sm_attributes, references=sm_references
      )
-     # print(db.DBM.tables)
 
+     # Inserting data
+     #   Osp
+     db_obj.insert(osp_name, osp_data)
+     #   sm
+     db_obj.insert(sm_name, sm_data)
+
+     # # Test last insert
+     # db_obj.check_specific_insert(
+     #      osp_name, osp_data
+     # )
+
+     # Query for task
+     
+
+
+     # print(db.DBM.tables)
+     db_obj.close()
